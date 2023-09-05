c82c97c7444bb071c2535eb160cdf2c5d4d31875
adallard <antonin.dallard@lirmm.fr>
2023-09-05 09:50:57 +0200

    first working sim

--- src/BiRobotTeleoperation.cpp
+++ src/BiRobotTeleoperation.cpp
@@ -1,20 +1,78 @@
 #include "BiRobotTeleoperation.h"
+#include <mc_rbdyn/RobotLoader.h>
+
+#include "yaml_path.h"
 
 BiRobotTeleoperation::BiRobotTeleoperation(mc_rbdyn::RobotModulePtr rm, double dt, const mc_rtc::Configuration & config)
 : mc_control::fsm::Controller(rm, dt, config)
 {
+  // std::cout << config_("states").dump() << std::endl;
+  // std::cout << "//" << std::endl;
+  // config_.load(mc_rtc::Configuration(BiRobotTask_CONFIG_PATH));
+
+  // std::cout << config_("states").dump() << std::endl;
+
+  external_robots_ = mc_rbdyn::Robots::make();
+
+  datastore().make<mc_rbdyn::RobotsPtr>("external_robots",external_robots_);
+  
+  mc_rbdyn::Robot & human_1 = robots().robot("human_1");
+  mc_rbdyn::Robot & human_2 = robots().robot("human_2");
+  bilateralTeleop::HumanPose hp_1;
+  bilateralTeleop::HumanPose hp_2;
+  updateHumanPose(human_1,hp_1);
+  updateHumanPose(human_2,hp_2);
+  datastore().make<bilateralTeleop::HumanPose>("human_1",hp_1);
+  datastore().make<bilateralTeleop::HumanPose>("human_2",hp_2);
 
   mc_rtc::log::success("BiRobotTeleoperation init done ");
 }
 
 bool BiRobotTeleoperation::run()
 {
+
+  mc_rbdyn::Robot & human_1 = robots().robot("human_1");
+  mc_rbdyn::Robot & human_2 = robots().robot("human_2");
+
+  bilateralTeleop::HumanPose hp_1;
+  bilateralTeleop::HumanPose hp_2;
+  updateHumanPose(human_1,hp_1);
+  updateHumanPose(human_2,hp_2);
+  datastore().assign<bilateralTeleop::HumanPose>("human_1",hp_1);
+  datastore().assign<bilateralTeleop::HumanPose>("human_2",hp_2);
+
   return mc_control::fsm::Controller::run();
 }
 
 void BiRobotTeleoperation::reset(const mc_control::ControllerResetData & reset_data)
 {
+  mc_rbdyn::Robot & robot_2 = robots().robot("robot_2");
+
+  mc_rbdyn::Robot & human_1 = robots().robot("human_1");
+  mc_rbdyn::Robot & human_2 = robots().robot("human_2");
+
+
+  robot_2.posW(sva::PTransformd(sva::RotZ(M_PI), Eigen::Vector3d(-0.6, 0., 0)) * alignFeet(robot(),"Foot",robot_2,"Foot") );
+
+  
+  human_1.posW(sva::PTransformd(sva::RotZ(M_PI), Eigen::Vector3d(0.6, 0., 0)) * alignFeet(robot_2,"Foot",human_1,"Sole") );
+  human_2.posW(sva::PTransformd(sva::RotZ(M_PI), Eigen::Vector3d(0.6, 0., 0)) * alignFeet(robot(),"Foot",human_2,"Sole") );
+
   mc_control::fsm::Controller::reset(reset_data);
 }
 
+sva::PTransformd alignFeet(const mc_rbdyn::Robot & robot_1,const std::string & surfaceSuffix_1, const mc_rbdyn::Robot & robot_2,const std::string & surfaceSuffix_2)
+{
+  const sva::PTransformd X_0_Fc1 = sva::PTransformd(robot_1.posW().rotation(),0.5 * ( robot_1.frame("Left" + surfaceSuffix_1).position().translation() 
+                                                                                  + robot_1.frame("Right" + surfaceSuffix_1).position().translation()));
+
+
+  const sva::PTransformd X_0_Fc2 = sva::PTransformd(robot_2.posW().rotation(),0.5 * ( robot_2.frame("Left" + surfaceSuffix_2).position().translation() 
+                                                                                  + robot_2.frame("Right" + surfaceSuffix_2).position().translation()));
+
+  const sva::PTransformd X_b2_fc2 = X_0_Fc2 * robot_2.posW().inv();
+
+  return X_b2_fc2.inv() * X_0_Fc1;
+
+}
 
--- src/BiRobotTeleoperation.h
+++ src/BiRobotTeleoperation.h
@@ -1,9 +1,10 @@
 #pragma once
 
 #include <mc_control/mc_controller.h>
+#include <mc_control/MCController.h>
 #include <mc_control/fsm/Controller.h>
-
 #include "api.h"
+#include <bilateralteleop/HumanRobotPose.h>
 
 struct BiRobotTeleoperation_DLLAPI BiRobotTeleoperation : public mc_control::fsm::Controller
 {
@@ -12,4 +13,49 @@ struct BiRobotTeleoperation_DLLAPI BiRobotTeleoperation : public mc_control::fsm
   bool run() override;
 
   void reset(const mc_control::ControllerResetData & reset_data) override;
+
+  mc_tasks::MetaTask* getTask(const std::string name)
+  {
+    for (auto & t : solver().tasks())
+    {
+      if(t->name() == name)
+      {
+        return t;
+      }
+    }
+    mc_rtc::log::error_and_throw<std::runtime_error>("tasks {} is not in the solver",name);
+  }
+
+  void updateHumanLink(const mc_rbdyn::Robot & human,const std::string & link ,bilateralTeleop::HumanPose & human_pose,const bilateralTeleop::Limbs human_link)
+  {
+    human_pose.setPose(human_link,human.bodyPosW(link));
+    sva::PTransformd X_link_link0 = sva::PTransformd( (human_pose.getPose(human_link).inv()).rotation(),Eigen::Vector3d::Zero() ); 
+    human_pose.setVel(human_link,X_link_link0 * human.bodyVelB(link));
+    human_pose.setAcc(human_link, X_link_link0  * human.bodyAccB(link));
+  }
+  void updateHumanPose(const mc_rbdyn::Robot & human ,bilateralTeleop::HumanPose & human_pose)
+  {
+    updateHumanLink(human,"LHandLink",human_pose,bilateralTeleop::Limbs::LeftHand);
+    updateHumanLink(human,"RHandLink",human_pose,bilateralTeleop::Limbs::RightHand);
+    updateHumanLink(human,"LForearmLink",human_pose,bilateralTeleop::Limbs::LeftForearm);
+    updateHumanLink(human,"RForearmLink",human_pose,bilateralTeleop::Limbs::RightForearm);
+    updateHumanLink(human,"LArmLink",human_pose,bilateralTeleop::Limbs::LeftArm);
+    updateHumanLink(human,"RArmLink",human_pose,bilateralTeleop::Limbs::RightArm);
+    updateHumanLink(human,"HipsLink",human_pose,bilateralTeleop::Limbs::Pelvis);
+
+  }
+
+private:
+  
+  mc_rbdyn::RobotsPtr external_robots_ = nullptr;
+
 };
+
+/**
+ * @brief Set robot_2 fb such as they have the same foot center 
+ * 
+ * @param robot_1 
+ * @param robot_2 
+ * @return sva::PTransformd 
+ */
+sva::PTransformd alignFeet(const mc_rbdyn::Robot & robot_1,const std::string & surfaceSuffix_1, const mc_rbdyn::Robot & robot_2,const std::string & surfaceSuffix_2);
