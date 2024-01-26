d0f207105c29428033f3f3bbe48e29b5a117c2c7
adallard <antonin.dallard@lirmm.fr>
2024-01-26 14:59:19 +0100

    add udp plugin and watiting function for udp control

--- src/states/BiRobotTeleoperation_Initial.cpp
+++ src/states/BiRobotTeleoperation_Initial.cpp
@@ -9,13 +9,37 @@ void BiRobotTeleoperation_Initial::configure(const mc_rtc::Configuration & confi
 void BiRobotTeleoperation_Initial::start(mc_control::fsm::Controller & ctl_)
 {
   auto & ctl = static_cast<BiRobotTeleoperation &>(ctl_);
+  ctl.datastore().make<bool>("robot_1_IsReady", false);
+  run(ctl_);
+
 }
 
-bool BiRobotTeleoperation_Initial::run(mc_control::fsm::Controller & ctl_)
+bool BiRobotTeleoperation_Initial::run(mc_control::fsm::Controller & ctl)
 {
-  auto & ctl = static_cast<BiRobotTeleoperation &>(ctl_);
   output("OK");
+  if(ctl.datastore().has("UDPPlugin"))
+  { // When using the UDP plugin wait until hrp4 is ready
+    const std::string datastore_func_name = "UDPPlugin::robot_1::reset";
+    if(!ctl.datastore().has(datastore_func_name))
+    {
+      ctl.datastore().make_call(datastore_func_name,
+                            [this,&ctl]()
+                            {
+                              mc_rtc::log::warning("TestUDPPugin::reset called, resetting posture task");
+                              ctl.datastore().assign<bool>("robot_1_IsReady", true);
+                              ctl.getPostureTask("robot_1")->reset();
+                            });
+      mc_rtc::log::info("waiting for robot_1 on udp");
+
+    }
+    return ctl.datastore().get<bool>("robot_1_IsReady");
+  }
+
   return true;
+
+
+
+  
 }
 
 void BiRobotTeleoperation_Initial::teardown(mc_control::fsm::Controller & ctl_)
